6b01075f31918189dd383fd67eaaf1795ab66faa
steven hostetter <steven.hostetter@brightmls.com>
2020-12-11 13:07:29 -0500

    adding changes for app.matrixclientnotifications

--- App.MatrixClientNotifications/App.MatrixClientNotifications/DeployScripts/afterInstall.ps1
+++ App.MatrixClientNotifications/App.MatrixClientNotifications/DeployScripts/afterInstall.ps1
@@ -10,27 +10,27 @@ switch ( $env:DEPLOYMENT_GROUP_NAME )
 {
     "aue1d1z1cdg${deploymentGroupAppName}developdeploy" {
         Rename-Item -path "$rootDir\app.dev.config" -NewName App.MatrixClientNotifications.exe.config -Force
-        Get-SSMParameter -WithDecryption 1 -Name "/secure/aue1/d1/dotnetiis/${parameterStoreAppName}/connectionstrings" | Select -ExpandProperty Value | Out-File -FilePath "$rootDir\ConnectionStrings.config"
+        Get-SSMParameter -WithDecryption 1 -Name "/secure/aue1/d1/dotnetjob/${parameterStoreAppName}/connectionstrings" | Select -ExpandProperty Value | Out-File -FilePath "$rootDir\ConnectionStrings.config"
     }
     "auw2d1z1cdg${deploymentGroupAppName}developdeploy" {
         Rename-Item -path "$rootDir\app.dev.config" -NewName App.MatrixClientNotifications.exe.config -Force
-        Get-SSMParameter -WithDecryption 1 -Name "/secure/auw2/d1/dotnetiis/${parameterStoreAppName}/connectionstrings" | Select -ExpandProperty Value | Out-File -FilePath "$rootDir\ConnectionStrings.config"
+        Get-SSMParameter -WithDecryption 1 -Name "/secure/auw2/d1/dotnetjob/${parameterStoreAppName}/connectionstrings" | Select -ExpandProperty Value | Out-File -FilePath "$rootDir\ConnectionStrings.config"
     }
     "aue1t1z1cdg${deploymentGroupAppName}releasedeploy" {
         Rename-Item -path "$rootDir\app.test.config" -NewName App.MatrixClientNotifications.exe.config -Force
-        Get-SSMParameter -WithDecryption 1 -Name "/secure/aue1/t1/dotnetiis/${parameterStoreAppName}/connectionstrings" | Select -ExpandProperty Value | Out-File -FilePath "$rootDir\ConnectionStrings.config"
+        Get-SSMParameter -WithDecryption 1 -Name "/secure/aue1/t1/dotnetjob/${parameterStoreAppName}/connectionstrings" | Select -ExpandProperty Value | Out-File -FilePath "$rootDir\ConnectionStrings.config"
     }
     "auw2t1z1cdg${deploymentGroupAppName}releasedeploy" {
         Rename-Item -path "$rootDir\app.test.config" -NewName App.MatrixClientNotifications.exe.config -Force
-        Get-SSMParameter -WithDecryption 1 -Name "/secure/auw2/t1/dotnetiis/${parameterStoreAppName}/connectionstrings" | Select -ExpandProperty Value | Out-File -FilePath "$rootDir\ConnectionStrings.config"
+        Get-SSMParameter -WithDecryption 1 -Name "/secure/auw2/t1/dotnetjob/${parameterStoreAppName}/connectionstrings" | Select -ExpandProperty Value | Out-File -FilePath "$rootDir\ConnectionStrings.config"
     }
     "aue1p1z1cdg${deploymentGroupAppName}releasedeploy" {
         Rename-Item -path "$rootDir\app.prod.config" -NewName App.MatrixClientNotifications.exe.config -Force
-        Get-SSMParameter -WithDecryption 1 -Name "/secure/aue1/p1/dotnetiis/${parameterStoreAppName}/connectionstrings" | Select -ExpandProperty Value | Out-File -FilePath "$rootDir\ConnectionStrings.config"
+        Get-SSMParameter -WithDecryption 1 -Name "/secure/aue1/p1/dotnetjob/${parameterStoreAppName}/connectionstrings" | Select -ExpandProperty Value | Out-File -FilePath "$rootDir\ConnectionStrings.config"
     }
     "auw2p1z1cdg${deploymentGroupAppName}releasedeploy" {
         Rename-Item -path "$rootDir\app.prod.config" -NewName App.MatrixClientNotifications.exe.config -Force
-        Get-SSMParameter -WithDecryption 1 -Name "/secure/auw2/p1/dotnetiis/${parameterStoreAppName}/connectionstrings" | Select -ExpandProperty Value | Out-File -FilePath "$rootDir\ConnectionStrings.config"
+        Get-SSMParameter -WithDecryption 1 -Name "/secure/auw2/p1/dotnetjob/${parameterStoreAppName}/connectionstrings" | Select -ExpandProperty Value | Out-File -FilePath "$rootDir\ConnectionStrings.config"
     }
     default { Write-Warning "$env:DEPLOYMENT_GROUP_NAME not valid." }
 }
